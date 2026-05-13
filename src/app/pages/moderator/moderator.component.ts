527339e8aa4ea2211daf5c18dda4ae155431d4d8
shweta kumari <160194777+Shweta-Kumari-34@users.noreply.github.com>
2026-05-13 21:46:59 +0530

    Delete src/app/pages/moderator directory

--- src/app/pages/moderator/moderator.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-moderator',
-  imports: [],
-  templateUrl: './moderator.component.html',
-  styleUrl: './moderator.component.scss',
-})
-export class ModeratorComponent {}
